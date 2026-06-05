37bd8b09041a62dfe9e246908b58b2f1b3105081
Alok Verma <aadiix23@gmail.com>
2026-06-06 00:08:25 +0530

    feat: implement authentication service with register and login functionality

---
+++ Authentication(7th Project)/src/service/authService.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcrypt");
+const appError = require("../utils/appError")
+const logger = require("../utils/logger")
+const respond = require("../utils/respond")
+const pool = require("../config/db")
+
+const register = async({name,email,password})=>{
+    let existing = await pool.query(
+        "SELECT EMAIL FROM users WHERE email= $1",[email]
+    );
+    if(existing.rows.length>0){
+        throw new appError("Email Already Registered",409)
+    }
+    const passwordHash = await bcrypt.hash(password,12)
+    const {rows}= await pool.query(`
+        INSERT INTO users(name,email,password_hash)
+        VALUES($1,$2,$3)
+        RETURNING id,name,email,created_at
+        `,[name,email,passwordHash]);
+
+        const user = rows[0];
+        logger.info("User Registered")
+        return user;
+}
+
+const login = async(email,password)=>{
+   const existingUser = await pool.query(
+       "SELECT email,password_hash FROM users WHERE email = $1",[email]
+   );
+   if(existingUser.rows.length===0){
+    throw new appError("Account is Not Registered",401)
+   }
+   const user = existingUser.rows[0];
+   const isMatch = bcrypt.compare(password,user.password_hash)
+   if(!isMatch){
+    throw new appError("Invalid Credentials",401)
+   }
+   logger.info("User LoggedIN",{
+    userId:user.id
+   })
+   return user;
+
+
+}
